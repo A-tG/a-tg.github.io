2c28c2c527d77ba2440856aef02803fe783bc98c
A-tG <a3tg.developer@gmail.com>
2024-05-19 18:45:39 +0300

    global rework

--- README.md
+++
@@ -1,1 +0,0 @@
-https://github.com/A-tG/guitar-site
--- js/worker_msg.js
+++
@@ -1,1 +0,0 @@
-msgActionEnum={metr:{startTicking:1,stopTicking:2,getTickrate:3,tickrate:4,tick:5}};function workerMsg(a,b){this.action=a;this.data=b};
